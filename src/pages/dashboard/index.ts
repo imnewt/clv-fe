5ba57fbab7f4a64522405f0b26218b475057d847
imnewtt <51883259+imnewt@users.noreply.github.com>
2023-08-15 16:05:08 +0700

    save token on login with google

--- src/pages/dashboard/index.ts
+++ src/pages/dashboard/index.ts
@@ -1,4 +1,22 @@
+import { setAccessToken } from "@/utils/functions";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Dashboard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { query } = router;
+    const { access_token } = query;
+    if (access_token) {
+      setAccessToken(access_token as string);
+      router.replace({
+        pathname: router.pathname,
+        query: {},
+      });
+    }
+  }, [router]);
+
   return "dashboard";
 };
 
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -8,3 +8,24 @@ export const isLoggedIn = () => {
   }
   return !!localStorage?.getItem(ACCESS_TOKEN_KEY);
 };
+
+export const getAccessToken = () => {
+  if (isServer) {
+    return;
+  }
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+};
+
+export const setAccessToken = (accessToken: string) => {
+  if (isServer) {
+    return;
+  }
+  return localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+};
+
+export const removeAccessToken = () => {
+  if (isServer) {
+    return;
+  }
+  return localStorage.removeItem(ACCESS_TOKEN_KEY);
+};
